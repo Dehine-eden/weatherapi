c5941d489170e7b1bb9e39a659950fd2d49c2da0
Dehine-eden <119399020+Dehine-eden@users.noreply.github.com>
2023-01-03 09:01:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+
+const https = require("https");
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+app.get ("/", function(req, res){
+
+https.get(url, function(response){
+	console.log(response.statusCode);
+
+    response.on("data", function(data){
+    const weatherData = JSON.parse(data);
+    const weatherDescription = weatherData.weather[0].description;
+    const object = {
+        name: "Dehine",
+        food: "Firfir"
+    }
+
+    console.log(JSON.stringify(object));
+    console.log(weatherData);
+
+
+    app.listen(3000, function(){
+        console.log("Server is running on port 3000.");
+    });
+
+        res.write("The temprature currently here is " + temp + "℃");
+        res.write("The weather can be described as " + weatherDescription);
+        res.send()
+    });
+
+});
+
+});
+
