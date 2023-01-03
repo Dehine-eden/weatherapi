771060dda8ff7d990fd1294e221ce3af90ae20ac
Dehine-eden <119399020+Dehine-eden@users.noreply.github.com>
2023-01-03 16:29:27 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 //jshint esversion:6
 
 const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
 
-const https = require("https");
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get ("/", function(req, res){
+    res.sendFile( __dirname + "/index.html");
+});
+
+app.post ("/", function(req, res){
+
+const query = req.body.cityName;
+const apikey = "4b01458a2b74d57ff37f136f382ac4d5";
+const unit = "metric"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
 
 https.get(url, function(response){
-	console.log(response.statusCode);
 
     response.on("data", function(data){
     const weatherData = JSON.parse(data);
+    console.log(weatherData);
+    const temp = weatherData.main.temp;
     const weatherDescription = weatherData.weather[0].description;
     const icon = weatherData.weather[0].icon;
     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    const object = {
-        name: "Dehine",
-        food: "Firfir"
-    }
 
-    console.log(JSON.stringify(object));
-    console.log(weatherData);
-
-
-    app.listen(3000, function(){
-        console.log("Server is running on port 3000.");
-    });
-
-        res.write("The temprature currently here is " + temp + "℃");
-        res.write("The weather can be described as " + weatherDescription);
+        res.write("The temprature currently in "+ query + " is " + temp + " degree celcius.");
+        res.write(" The weather can be described as " + weatherDescription);
+        //res.set("Content-Type", "text/html");
         res.write("<img src=" + imageURL + ">");
-        res.send("hey there");
+        res.send();
     });
 
 });
+
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
 
 });
 
